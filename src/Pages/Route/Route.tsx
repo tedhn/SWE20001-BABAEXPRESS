16f619982502d4bab7160133ee61962849a6cd0b
Ted <heinhtetnaing186@gmail.com>
2024-05-20 07:04:14 +0800

    (refactor) added confirmation box for delete route button

--- src/Pages/Route/Route.tsx
+++ src/Pages/Route/Route.tsx
@@ -3,10 +3,11 @@ import { useAirTable } from "~/Context/AirTableContext";
 import { RouteType } from "~/type";
 import { useNavigate } from "react-router-dom";
 import { formatDate } from "~/utils";
-import { ActionIcon, Button, Paper, Text } from "@mantine/core";
+import { ActionIcon, Button, Modal, Paper, Text } from "@mantine/core";
 import { useUser } from "~/Context/UserContext";
 import { IconTrash } from "@tabler/icons-react";
 import toast from "react-hot-toast";
+import { useDisclosure } from "@mantine/hooks";
 
 const Route = () => {
   const navigate = useNavigate();
@@ -15,6 +16,8 @@ const Route = () => {
 
   const [routes, setRoutes] = useState<RouteType[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [opened, { open, close }] = useDisclosure(false);
+  const [selectedRouteId, setSelectedRoutedId] = useState<string>("");
 
   useEffect(() => {
     setIsLoading(true);
@@ -29,16 +32,19 @@ const Route = () => {
   const handleNavigate = (routeId: string) => {
     navigate(`/routes/${routeId}`);
   };
-  const handleDelete = async (routeId: string) => {
-    const { success } = await deleteRoute(routeId);
-    console.log(success);
+  const handleDelete = async () => {
+    setIsLoading(true);
+    const { success } = await deleteRoute(selectedRouteId);
 
     if (success) {
-      setRoutes(routes.filter((r) => r.route_Id !== routeId));
+      setRoutes(routes.filter((r) => r.route_Id !== selectedRouteId));
       toast.success("Successfully deleted");
     } else {
       toast.error("Delete failed");
     }
+
+    close();
+    setIsLoading(false);
   };
 
   if (isLoading) {
@@ -67,11 +73,48 @@ const Route = () => {
           <RouteCard
             route={route}
             handleNavigate={handleNavigate}
-            handleDelete={handleDelete}
+            openDeleteConfirmationModal={() => {
+              open();
+              setSelectedRoutedId(route.route_Id);
+            }}
             key={route.route_Id}
           />
         ))}
       </div>
+
+      <Modal
+        title="Confirm Deletion"
+        opened={opened}
+        onClose={close}
+        size="sm"
+        overlayProps={{ opacity: "0.7" }}
+      >
+        <div className="text-center">
+          <p className="text-gray-700 mb-4">
+            Are you sure you want to delete this item?
+          </p>
+          <div className="flex justify-center">
+            <Button
+              onClick={close}
+              variant="outline"
+              color="gray"
+              disabled={isLoading}
+            >
+              Cancel
+            </Button>
+            <Button
+              onClick={() => handleDelete()}
+              variant="outline"
+              color="red"
+              loading={isLoading}
+              disabled={isLoading}
+              className="ml-2"
+            >
+              Delete
+            </Button>
+          </div>
+        </div>
+      </Modal>
     </div>
   );
 };
@@ -79,11 +122,11 @@ const Route = () => {
 const RouteCard = ({
   route,
   handleNavigate,
-  handleDelete,
+  openDeleteConfirmationModal,
 }: {
   route: RouteType;
   handleNavigate: (route_id: string) => void;
-  handleDelete: (route_id: string) => void;
+  openDeleteConfirmationModal: () => void;
 }) => {
   const { user } = useUser();
 
@@ -94,7 +137,7 @@ const RouteCard = ({
           variant="white"
           color="red"
           className="absolute top-5 right-5 rounded-md hover:bg-gray-200 w-fit"
-          onClick={() => handleDelete(route.route_Id)}
+          onClick={() => openDeleteConfirmationModal()}
         >
           <IconTrash style={{ width: "70%", height: "70%" }} stroke={2} />
         </ActionIcon>
